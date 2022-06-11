bc1481095679b15ac638e83fa2f7f73f8c49e13f
Gabriel Pozato <99850728+GPozato@users.noreply.github.com>
2022-06-10 21:07:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function adicionarFilme(){
+    var filmeFavorito= document.getElementById("filme").value;
+    if (filmeFavorito.endsWith(".jpg")) {
+        listaFilmesNaTela(filmeFavorito);
+    } else{
+        console.error("Endereço de filme invalido");
+    }
+    document.getElementById("filme").value="";
+}
+function listaFilmesNaTela(filme){
+    console.log(filme)
+    var elementoFilmeFavorito= "<img src=" + filme + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+}
